2bb57e3adca49362704469a0163dde2fb9495c68
ramya-jayaram-core201 <ramya15.j@gmail.com>
2021-06-24 17:08:18 +0530

    Create The ItemController - 1

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -39,7 +39,7 @@ namespace WishList.Controllers
 
         public IActionResult Delete(int Id)
         {
-            Item item = _context.Items.Where(x => x.Id == Id).FirstOrDefault();
+            var item = _context.Items.FirstOrDefault(x => x.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
